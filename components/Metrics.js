1a428a20f1ae87547ce215e7900109368ba66002
Kowsheek Mahmood <kowsheek@gmail.com>
2022-07-12 01:33:19 -0400

    add ETH to USD conversion

--- components/Metrics.js
+++ components/Metrics.js
@@ -1,17 +1,33 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import BigNumber from 'bignumber.js';
+const { getEthPriceNow }= require('get-eth-price');
+const pow = BigNumber(10).pow(18);
 
 export default function Metrics({ totalCount, totalVolume }) {
 
-    const totalVolumeDisplay = useMemo(() => {
-        const pow = BigNumber(10).pow(18);
-        return BigNumber(totalVolume).div(pow).toString();
-    }, [totalVolume]);
+    const [totalETHVolumeDisplay, setTotalETHVolumeDisplay] = React.useState(0);
+    const [totalUSDVolumeDisplay, setTotalUSDVolumeDisplay] = React.useState(0);
 
+    const totalVolumeBN = useMemo(() => {
+        const bn = BigNumber(totalVolume).div(pow);
+        setTotalETHVolumeDisplay(bn.toFixed(2));
+        return bn;
+    }, [totalVolume]);
+    
+    useEffect(async () => {
+        async function getPrice() {
+            const priceResponse = await getEthPriceNow()
+            const entryKey = Object.keys(priceResponse)[0]
+            const usdConversion = priceResponse[entryKey].ETH.USD
+            setTotalUSDVolumeDisplay((totalVolumeBN * usdConversion).toFixed(2))
+        }
+        getPrice()
+    }, [totalVolumeBN]);
 
     return <div>
         <h2 className="title">{totalCount} NFTs</h2>
-        <h2 className="title">{totalVolumeDisplay} ETH Volume</h2>
+        <h2 className="title">{totalETHVolumeDisplay} ETH Volume</h2>
+        <h2 className="title">${totalUSDVolumeDisplay} USD Volume</h2>
     </div>
   }
   
